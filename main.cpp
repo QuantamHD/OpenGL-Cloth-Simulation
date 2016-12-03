240eb1718a18dad59147b83d166a2571bbfd2da9
Ethan Mahintorabi <emahintorabi@ada.gonzaga.edu>
2016-12-03 15:31:35 -0800

    The initial Commit

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#ifdef __APPLE__
+#  include <GL/glew.h>
+#  include <GL/freeglut.h>
+#  include <OpenGL/glext.h>
+#else
+#  include <GL/glew.h>
+#  include <GL/freeglut.h>
+#  include <GL/glext.h>
+#pragma comment(lib, "glew32.lib")
+#endif
+
+#include<iostream>
+
+double delta = 0;
+long currentTime = 0;
+
+void calculateDeltaTime(){
+    long newTime = glutGet(GLUT_ELAPSED_TIME);
+    delta = (newTime - currentTime)/1000.0;
+    currentTime = newTime;
+}
+
+void update(){
+    std::cout << "Time per frame in seconds " << delta << std::endl;
+}
+
+void animate(int value){
+    update();
+    glutPostRedisplay();
+    glutTimerFunc(8,animate, 0);
+}
+
+
+
+void init(){
+    glEnable(GL_DEPTH_TEST);
+    currentTime = glutGet(GLUT_ELAPSED_TIME);
+}
+
+void resizeViewport(int width, int height){
+    glViewport(0,0,width, height);
+}
+
+void display(){
+    glClearColor(0.5019, 0.8, 0.8,1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    calculateDeltaTime();
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char **argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH );
+    glutInitContextVersion( 3, 2 );
+    glutInitContextProfile( GLUT_CORE_PROFILE );
+
+    glutInitWindowSize(720,720);
+    glutCreateWindow( "Cloth Simulation" );
+
+    glewInit();
+    init();
+
+    glutTimerFunc(8, animate, 0);
+    glutDisplayFunc(display);
+    glutReshapeFunc(resizeViewport);
+    glutMainLoop();
+
+    return 0;
+}
